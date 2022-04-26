671178aaddb4c5109f81fd5584a158f45794abcd
steven.han <steven.han@cgi.com>
2022-04-26 11:22:29 -0400

    More user friendly error messages

--- src/app/shared/services/abstract-api.service.ts
+++ src/app/shared/services/abstract-api.service.ts
@@ -10,16 +10,7 @@ export class AbstractApiService {
   constructor(protected httpClient: HttpClient) { }
 
   handleError(err: HttpErrorResponse): Observable<never> {
-    let errorMessage: string;
-    if (err.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      errorMessage = `An error occurred: ${err.error.message}`;
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      errorMessage = `Backend returned code ${err.status}: ${err.message}`;
-    }
     console.error(err);
-    return throwError(() => errorMessage);
+    return throwError(() => err.error.error);
   }
 }
